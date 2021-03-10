40d1d89b2cfc66e760de355892ea191b7d3fa73c
illuminator777 <ikolomiytsev37@gmail.com>
2021-03-10 17:09:55 +0200

    end work, don't done

--- hw#29/js/my.js
+++ hw#29/js/my.js
@@ -4,8 +4,19 @@ const alertMenu = document.querySelector('.alert');
 
 function ButtonClose() {
 
-    alertMenu.classList.add('displ-none')
+    alertMenu.classList.remove('show')
 
 }
 
 closeBtn.addEventListener('click', ButtonClose);
+
+
+const closeBtn = document.querySelector('.btn-close');
+const alertMenu = document.querySelector('.alert');
+
+closeBtn.forEach((allBtnClose) => {
+
+    allBtnClose.addEventListener('click', function (event) {
+        event.target.classList.add('displ-none')
+    });
+});
