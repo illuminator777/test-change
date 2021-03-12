e3727fc713920d90040e20904131b0e8243b999d
illuminator777 <ikolomiytsev37@gmail.com>
2021-03-12 12:00:33 +0200

    done work

--- hw#29/js/my.js
+++ hw#29/js/my.js
@@ -1,22 +1,4 @@
-const closeBtn = document.querySelector('.btn-close');
-const alertMenu = document.querySelector('.alert');
-
-
-function ButtonClose() {
-
-    alertMenu.classList.remove('show')
-
+const funk = (id) => {
+    document.getElementById(id).classList.add('displ-none')
 }
 
-closeBtn.addEventListener('click', ButtonClose);
-
-
-const closeBtn = document.querySelectorAll('.btn-close');
-const alertMenu = document.querySelectorAll('.alert');
-
-closeBtn.forEach((allBtnClose) => {
-
-    allBtnClose.addEventListener('click', function (event) {
-        event.target.classList.add('displ-none')
-    });
-});
